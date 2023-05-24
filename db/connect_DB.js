ed960ef8ebc087bd7ec0830fedbe641b14756ecb
sWD4 <shubham2405@gmail.com>
2023-05-24 21:32:44 +0530

    db change

--- db/connect_DB.js
+++ db/connect_DB.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 const url = 'mongodb://127.0.0.1:27017/PRACTICEPORTAL'
-
+const live_url = 'mongodb+srv://sshubham2405:shubham1234@cluster0.yjekmrn.mongodb.net/admissionportal?retryWrites=true&w=majority'
 
 const connectDB=()=>{
-    return mongoose.connect(url)
+    return mongoose.connect(live_url)
 
 .then(()=>{
     console.log("Connected Successfully")
