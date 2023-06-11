c877542e9942133da39fee56dbf2253b179ade7a
pratham <prathamsharma4341@gmail.com>
2023-06-11 09:42:54 +0530

    contact change

--- controllers/FrontController.js
+++ controllers/FrontController.js
@@ -19,7 +19,7 @@ class FrontController {
     });
   };
 
-  static registration = async (req, res) => {
+ static registration = async (req, res) => {
     try {
       res.render("registration", { message: req.flash("error") });
     } catch (error) {
